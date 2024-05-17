cb3856fc7b45c0f88c28e9af252f8ed0070c53b8
TongZ <471205975@qq.com>
2024-05-17 17:27:41 +0800

    update windows api

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,49 +2,51 @@ import test from 'ava'
 
 import { readRegistry, writeRegistry, deleteRegistry, getProcessExists } from '../index'
 
+const key = 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings'
 if (process.platform === 'win32') {
-  test('write proxy', (t) => {
-    writeRegistry(
-      0,
-      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings',
-      'ProxyServer',
-      0,
-      '127.0.0.1:29613',
-    )
-    const res = readRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings', 'ProxyServer', 0)
+  test('write|read reg_sz', (t) => {
+    writeRegistry(0, key, 'ProxyServer', '127.0.0.1:29613')
+    const res = readRegistry(0, key, 'ProxyServer')
     t.is(res, '127.0.0.1:29613')
   })
 
-  test('delete proxy', (t) => {
-    deleteRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings', 'ProxyServer')
-    const res = readRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings', 'ProxyServer', 0)
-    t.is(res, null)
+  test('write|read dword', (t) => {
+    writeRegistry(0, key, 'dwd', 1)
+    const res = readRegistry(0, key, 'dwd')
+    t.is(res, 1)
   })
 
-  test('write read reg', (t) => {
-    writeRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test', 0, 'test')
-    const res = readRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test', 0)
-    t.is(res, 'test')
+  test('write|read qword', (t) => {
+    writeRegistry(0, key, 'qwd', 100)
+    const res = readRegistry(0, key, 'qwd')
+    t.is(res, 100)
   })
 
-  // test('write read reg path not exists', (t) => {
-  //   writeRegistry(0, 'SOFTWARE\\4e68d05e-43d6-5a2a-b5c0-61957baec133\\fuck', 'test', 0, 'test')
-  //   const res = readRegistry(0, 'SOFTWARE\\4e68d05e-43d6-5a2a-b5c0-61957baec133\\fuck', 'test', 0)
-  //   t.is(res, 'test')
-  // })
+  test('write|read multi_sz', (t) => {
+    writeRegistry(0, key, 'msz', ['1', '2'])
+    const res = readRegistry(0, key, 'msz') as string[]
+    t.assert(res.join('') === '12', 'res is 12')
+  })
 
-  test('delete reg', (t) => {
-    writeRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test', 0, 'test')
-    deleteRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test')
-    const err = t.throws(() => {
-      deleteRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test')
-    })
-    const res = readRegistry(0, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run', 'test', 0)
-    t.assert(err instanceof Error)
+  test('write|read binary', (t) => {
+    writeRegistry(0, key, 'binary', Buffer.from([1, 2, 3]))
+    const res = readRegistry(0, key, 'binary')
+    t.assert(typeof res === 'object', 'res is object')
+  })
+
+  test('delete', (t) => {
+    deleteRegistry(0, key, 'ProxyServer')
+    deleteRegistry(0, key, 'binary')
+    deleteRegistry(0, key, 'msz')
+    deleteRegistry(0, key, 'qwd')
+    deleteRegistry(0, key, 'dwd')
+    const res = readRegistry(0, key, 'ProxyServer')
     t.is(res, null)
+    t.throws(() => deleteRegistry(0, key, 'ProxyServer'))
   })
 
   test('get process exists', (t) => {
-    t.is(getProcessExists('node.exe'), true)
+    t.is(getProcessExists('node', false), true)
+    t.is(getProcessExists('node.exe', true), true)
   })
 }
--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,21 +1,70 @@
+import { execSync } from 'node:child_process'
+
 import b from 'benny'
 
-import { plus100 } from '../index'
-
-function add(a: number) {
-  return a + 100
-}
+import { getProcessExists, readRegistry, writeRegistry, deleteRegistry } from '../index'
 
 async function run() {
   await b.suite(
-    'Add 100',
+    'getProcessExists node.exe',
 
-    b.add('Native a + 100', () => {
-      plus100(10)
+    b.add('Native getProcessExists exact node.exe', () => {
+      getProcessExists('node.exe', true)
     }),
 
-    b.add('JavaScript a + 100', () => {
-      add(10)
+    b.add('Native getProcessExists not exact node.exe', () => {
+      getProcessExists('node.exe', false)
+    }),
+
+    b.add('cmd getProcessExists node.exe', () => {
+      execSync('TASKLIST /FI "USERNAME ne NT AUTHORITY\\SYSTEM" /FI "STATUS eq running"  /FI "IMAGENAME eq node.exe"')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+
+  await b.suite(
+    'writeRegistry test',
+
+    b.add('Native writeRegistry test', () => {
+      writeRegistry(0, 'Console', 'test', 0)
+    }),
+
+    b.add('cmd writeRegistry test', () => {
+      execSync('reg add HKCU\\Console /v test /t REG_DWORD /d 0 /f')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+
+  await b.suite(
+    'readRegistry test',
+
+    b.add('Native readRegistry test', () => {
+      readRegistry(0, 'Console', 'test')
+    }),
+
+    b.add('cmd readRegistry test', () => {
+      execSync('reg query HKCU\\Console /v test')
+    }),
+
+    b.cycle(),
+    b.complete(),
+  )
+
+  await b.suite(
+    'deleteRegistry test',
+
+    b.add('Native deleteRegistry test', () => {
+      writeRegistry(0, 'Console', 'test', 0)
+      deleteRegistry(0, 'Console', 'test')
+    }),
+
+    b.add('cmd deleteRegistry test', () => {
+      writeRegistry(0, 'Console', 'test', 0)
+      execSync('reg delete HKCU\\Console /v test /f')
     }),
 
     b.cycle(),
